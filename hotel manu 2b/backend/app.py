0409ac416eacfbab9340152e37f46696243efdc2
Aprende a tirar a conta <emanuelly.lara.ferreira@escola.pr.gov.br>
2026-03-10 09:26:12 -0300

    Enhance app.py with database and Excel support
    Added database and Excel file handling capabilities.

--- hotel manu 2b/backend/app.py
+++ hotel manu 2b/backend/app.py
@@ -1,7 +1,7 @@
 import os
 from flask import Flask, send_from_directory
-
-# caminho da pasta p dois pontos mandam isso a 1 pasta atras onde esta o front.
+import openpyxl#Biblioteca para ler e escrever planilia Excel(.x
+from datetime inport
 BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__),".."))
 
 #pasta frontend
@@ -9,7 +9,14 @@ FRONTEND_DIR = os.path.join(BASE_DIR,"frontend")
 
 # pasta static (css)
 STATIC_DIR= os.path.join(BASE_DIR, "static")
-
+DB_DIR=os.path.join(os.path.dirname(_file_),",,","db"
+EXCEL_FILE=os,path.join(DB_DIR,"clientes.xlsx")
+CCOLUMNS=[
+"ID,
+"Nome",
+"CPF",
+"Telefone",
+"Endereço",
 app = Flask(__name__, static_folder=STATIC_DIR,static_url_path="/" + STATIC_DIR)
 
 @app.route("/")
@@ -28,6 +35,7 @@ def alterar_page ():
 
 
 app.run()
+
 
 
 
